7906c54e45053c7a071df129bab4c7669d6dcaf1
iamspd <iamspd.ai9@gmail.com>
2021-05-29 11:30:10 -0400

    MainActivity - Runnable implemented

--- app/src/main/java/com/example/timerdemo/MainActivity.java
+++ app/src/main/java/com/example/timerdemo/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.timerdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+
+                Log.i("InRunnable: ", "A second has passed!");
+
+                handler.postDelayed(this, 1000);
+            }
+        };
+
+        handler.post(runnable);
+
     }
 }
